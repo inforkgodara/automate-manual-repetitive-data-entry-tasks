05ce1d5961db742c0f81d2194977b459262fc0e3
Ramesh Kumar <50226667+inforkgodara@users.noreply.github.com>
2020-10-24 14:06:04 +0100

    First commit

---
+++ src/inforkgodara/SalesOrderRegister.java
@@ -0,0 +1,22 @@
+package inforkgodara;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class SalesOrderRegister extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sales_order_register.fxml"));
+        primaryStage.setTitle("Sales Order Register");
+        primaryStage.setScene(new Scene(root, 800, 500));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
